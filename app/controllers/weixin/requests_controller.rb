8d2582b8aab8fe7bf64413eacc107358d4cda132
apprentice1988 <zilongji@gmail.com>
2014-08-21 22:17:24 +0800

    fix bug

--- app/controllers/weixin/requests_controller.rb
+++ app/controllers/weixin/requests_controller.rb
@@ -1,6 +1,6 @@
 class Weixin::RequestsController < ApplicationController
 	def authentication
-		token = 1123
+		token = '1123'
 		str = [params[:timestamp], token, params[:nonce]].sort.join
 		sha = Digest::SHA1.hexdigest(str)
 		result = sha == params[:signature] ? params[:echostr] : false
